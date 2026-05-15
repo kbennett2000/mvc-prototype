242f661a7fd0e33807b0ab11c677dc278a6156fa
Kris Bennett <kbennett2000@hotmail.com>
2026-05-15 15:15:11 -0600

    sync

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/kb/Desktop/Projects/mvc-prototype/tina/__generated__/.cache/1778877883967', url: 'http://localhost:4001/graphql', token: '8d9251113037bef7e3f46d0ced3719e933b6a62a', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
 export default client;
   
